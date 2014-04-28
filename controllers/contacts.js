147ee5905e27ca237e937914910a60f3d96885d1
Tony Brown <tony.brown.357@gmail.com>
2014-04-28 03:40:05 -0400

    jshint

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,11 +3,12 @@ var models = require('../app/models')
 
 module.exports = {
     index: function(req, res) {
-        models.Contact.find({}, function(err, data) {
-            res.json(data);
-        });
-    },
-    getById: function(req, res) {
+      models.Contact.find({}, function(err, data) {
+        res.json(data);
+      });
+    }
+
+  , getById: function(req, res) {
         models.Contact.find({ _id: req.params.id }, function(err, contact) {
             if (err) {
                 res.json({error: 'Contact not found.'});
@@ -15,19 +16,20 @@ module.exports = {
                 res.json(contact);
             }
         });
-    },
-    add: function(req, res) {
-        var newContact = new models.Contact(req.body);
-        newContact.gravatar = md5(newContact.email);
-        newContact.save(function(err, contact) {
-            if (err) {
-                res.json({error: 'Error adding contact.'});
-            } else {
-                res.json(contact);
-            }
-        });
-    },
-    // update: function(req, res) {
+    }
+
+  , add: function(req, res) {
+      var newContact = new models.Contact(req.body);
+      newContact.gravatar = md5(newContact.email);
+      newContact.save(function(err, contact) {
+        if (err) {
+          res.json({error: 'Error adding contact.'});
+        } else {
+            res.json(contact);
+        }
+      });
+    }
+    //, update: function(req, res) {
     //     console.log(req.body);
     //     models.Contact.update({ _id: req.body.id }, req.body, function(err, updated) {
     //         if (err) {
@@ -36,16 +38,17 @@ module.exports = {
     //             res.json(updated);
     //         }
     //     })
-    // },
-    delete: function(req, res) {
-        models.Contact.findOne({ _id: req.params.id }, function(err, contact) {
-            if (err) {
-                res.json({error: 'Contact not found.'});
-            } else {
-                contact.remove(function(err, contact){
-                    res.json(200, {status: 'Success'});
-                });
-            }
-        });
-    }
+    // }
+
+, delete: function(req, res) {
+    models.Contact.findOne({ _id: req.params.id }, function(err, contact) {
+      if (err) {
+        res.json({error: 'Contact not found.'});
+      } else {
+          contact.remove(function(err, contact){
+            res.json(200, {status: 'Success'});
+          });
+      }
+    });
+  }
 };
